12f5d8fc82c8a3744885949b3f526527c73b693f
dimonck7 <dima.chuvardinsky@gmail.com>
2015-03-05 20:56:23 +0200

    Init

---
+++ myblog/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'prog.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^blog/', include('blog.urls')),
+    url(r'^blog/comingsoon/', include('blog.urls')),
+    url(r'^blog/contact/', include('blog.urls')),
+)
